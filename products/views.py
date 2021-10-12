9dc5ff300a47ed578a7995fc1d49f29c49a55f18
yuwinzer <yu.moskalev@gmail.com>
2021-10-12 20:30:23 +0300

    2 lesson done

--- products/views.py
+++ products/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'Geekshop'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {'title': 'Geekshop - catalog'}
+    return render(request, 'products/products.html', context)
