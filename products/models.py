0c0252495f1584ab53c86485511344b5f1b20d91
yuwinzer <yu.moskalev@gmail.com>
2021-10-15 01:21:33 +0300

    added superuser and models to admin panel

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -14,5 +17,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
 
 
