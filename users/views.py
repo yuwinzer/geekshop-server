f31e17e014a29aa47feff0a8257b4315c1916a1b
yuwinzer <yu.moskalev@gmail.com>
2021-10-15 23:31:41 +0300

    added user logout

--- users/views.py
+++ users/views.py
@@ -36,3 +36,7 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
