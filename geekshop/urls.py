7cee3dfa1ba4b84e67a5cc64385114fab30aefce
yuwinzer <yu.moskalev@gmail.com>
2021-10-15 21:21:44 +0300

    added login and registration httml

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+
 from products.views import index, products
 
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    context = {'title': 'Geekshop - Авторизация'}
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    context = {'title': 'Geekshop - Регистрация'}
+    return render(request, 'users/registration.html', context)
