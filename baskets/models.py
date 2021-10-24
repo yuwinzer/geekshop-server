0466ab9615084a05937800cbf0190dbf82095248
yuwinzer <yu.moskalev@gmail.com>
2021-10-25 01:16:00 +0300

    created controller basket_add, created basket model

--- baskets/models.py
+++ baskets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
---
+++ baskets/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from baskets.views import basket_add
+
+app_name = 'baskets'
+urlpatterns = [
+    path('add/<int:product_id>/', basket_add, name='index'),
+]
