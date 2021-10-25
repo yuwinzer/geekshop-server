5824cec1fdf5652ad73d9f1009c62d652aeb6a6d
yuwinzer <yu.moskalev@gmail.com>
2021-10-26 01:28:17 +0300

    added login_required

--- baskets/views.py
+++ baskets/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import Product
 from baskets.models import Basket
 
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from baskets.models import Basket
@@ -37,6 +38,8 @@ def registration(request):
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
 
+
+@login_required
 def profile(request):
     user = request.user
     # from users.models import User
