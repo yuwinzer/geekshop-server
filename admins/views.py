f564bb76fc9ed3f42611fc32e0a362d75c3f2f5f
yuwinzer <yu.moskalev@gmail.com>
2021-10-26 22:13:19 +0300

    created READ for admib users

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from users.models import User
+
 def index(request):
     context = {'title': 'GeekShop - Админ панель'}
     return render(request, 'admins/index.html', context)
@@ -10,7 +12,10 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи'}
+    context = {
+        'title': 'GeekShop - Пользователи',
+        'users': User.objects.all(),
+    }
     return render(request, 'admins/admin-users-read.html', context)
 
 def admin_users_update(request):
