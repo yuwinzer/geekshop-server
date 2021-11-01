ed90315b7797488ed30358767dc49338ca186efc
yuwinzer <yu.moskalev@gmail.com>
2021-11-01 11:25:02 +0200

    added categories filtering in products.html (lesson_8)

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from products.views import products
 
 app_name = 'products'
+
 urlpatterns = [
-    path('', products, name='index'),
+    path('', products, name='index'), # .../products/
+    path('<int:category_id>/', products, name='category'), # .../products/<category_id>/
 ]
